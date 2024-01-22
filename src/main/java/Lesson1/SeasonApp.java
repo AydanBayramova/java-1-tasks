8f5cc413e9bd21f86fec425c19d91680c2793113
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-22 20:18:05 +0400

    send

--- src/main/java/Lesson1/SeasonApp.java
+++ src/main/java/Lesson1/SeasonApp.java
@@ -7,15 +7,39 @@ public class SeasonApp {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int number = scanner.nextInt();
+        int i;
         if (number > 1 && number < 12) {
-            if (number >= 3 && number <= 5) {
-                System.out.println("spring");
-            } else if (number >= 6 && number <= 9) {
-                System.out.println("summer");
-            } else if (number >= 10 && number <= 11) {
-                System.out.println("autumn");
+            if (number == 1) {
+                System.out.println("january");
+            } else if (number == 2) {
+                System.out.println("february");
+
+            } else if (number == 3) {
+                System.out.println("mart");
+
+            } else if (number == 4) {
+                System.out.println("april");
+
+            } else if (number == 5) {
+                System.out.println("may");
+
+            } else if (number == 6) {
+                System.out.println("june");
+
+            } else if (number == 7) {
+                System.out.println("jule");
+
+            } else if (number == 8) {
+                System.out.println("august");
+
+            } else if (number == 9) {
+                System.out.println("sempember");
+
+            } else if (number == 10) {
+                System.out.println("november");
+
             } else {
-                System.out.println("winter");
+                System.out.println("december");
             }
         }
 
