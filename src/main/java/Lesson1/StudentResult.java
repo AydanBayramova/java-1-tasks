a3efc8b7d24d1f166bf8d6c6b01360cdc64cab58
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-22 20:51:56 +0400

    send

--- src/main/java/Lesson1/StudentResult.java
+++ src/main/java/Lesson1/StudentResult.java
@@ -6,25 +6,26 @@ public class StudentResult {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter exam result");
-        Double grade = scanner.nextDouble();
-        if (grade >= 91 && grade <= 100) {
-            System.out.println("student result A");
-        } else if (grade >= 81 && grade <= 90) {
-            System.out.println("student result b");
+        double grade = scanner.nextDouble();
+//        if (grade >= 91 && grade <= 100) {
+//            System.out.println("student result A");
+//        } else if (grade >= 81 && grade <= 90) {
+//            System.out.println("student result b");
+//
+//        } else if (grade >= 71 && grade <= 80) {
+//            System.out.println("student result C");
+//
+//        } else if (grade >= 61 && grade <= 70) {
+//            System.out.println("student result D");
+//
+//        } else if (grade >= 51 && grade <= 60) {
+//            System.out.println("student result E");
+//
+//        } else if (grade<51 && grade<=0) {
+//            System.out.println("failed");
+//        } else {
+//            System.out.println("wrong number");
+//        }
 
-        } else if (grade >= 71 && grade <= 80) {
-            System.out.println("student result C");
-
-        } else if (grade >= 61 && grade <= 70) {
-            System.out.println("student result D");
-
-        } else if (grade >= 51 && grade <= 60) {
-            System.out.println("student result E");
-
-        } else if (grade<51 && grade<=0) {
-            System.out.println("failed");
-        } else {
-            System.out.println("wrong number");
-        }
     }
 }
