f95475771f8a7461ce4778a8fe11f794dd9e0e0b
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-22 20:09:52 +0400

    send

--- src/main/java/Lesson1/SeasonApp.java
+++ src/main/java/Lesson1/SeasonApp.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class SeasonApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number:");
+        System.out.print("Enter the number: ");
         int number = scanner.nextInt();
-        String a;
-        String season = (number > 0 && (number == 12 || number == 1 || number == 2)) ? (a = "winter") :
-                ((number == 3 || number == 4 || number == 5) ? (a = "spring") :
-                        ((number == 6 || number == 7 || number == 8) ? (a = "summer") :
-                                ((number == 9 || number == 10 || number == 11) ? (a = "winter") : (a = "Ele bir fesil yokkkk :)"))));
-
-        System.out.println(season);
+        if (number > 1 && number < 12) {
+            if (number >= 3 && number <= 5) {
+                System.out.println("spring");
+            } else if (number >= 6 && number <= 9) {
+                System.out.println("summer");
+            } else if (number >= 10 && number <= 11) {
+                System.out.println("autumn");
+            } else {
+                System.out.println("winter");
+            }
+        }
 
     }
 }
