bf8732b951918f779268cffcc617297c453f8fea
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-22 19:39:06 +0400

    send

--- src/main/java/Lesson1/WordApp.java
+++ src/main/java/Lesson1/WordApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class WordApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("write word");
+        System.out.println("write words");
         String word = scanner.nextLine();
         System.out.println(word.length() * 2);
     }
