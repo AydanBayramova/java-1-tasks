5a531f71613fe1cda1a20ad7533b212a293edcf8
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-26 20:14:51 +0400

    send

---
+++ src/main/java/TwoMoreDimArrays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TwoMoreDimArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Eded yazin");
+        int number = scanner.nextInt();
+        int array[][] = new int[number][number];
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.println("hjhij");
+            }
+        }
+        System.out.println();
+
+    }
+}
