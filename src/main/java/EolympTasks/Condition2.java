80bdb1510299031777fb39d75932bc39f7cf889b
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-28 11:53:16 +0400

    send

--- src/main/java/EolympTasks/Condition2.java
+++ src/main/java/EolympTasks/Condition2.java
@@ -5,33 +5,11 @@ import java.util.Scanner;
 public class Condition2 {
     public static void main(String[] args) {
         int x = new Scanner(System.in).nextInt();
-
-
-        if (x < Math.pow(-10, 4) || x > Math.pow(10, 4)) {
-            if (x >= 10) {
-                System.out.println( (int) (Math.pow(x, 3) + 5 * x));
-            } else {
-                System.out.println( (int) (Math.pow(x, 2) - 2 * x + 4));
-            }
+        if (x >= 10) {
+            System.out.println((long) (Math.pow(x, 3) + 5 * x));
+        } else {
+            System.out.println((long) (Math.pow(x, 2) - 2 * x + 4));
         }
-//        int y=0;  //=(int) (Math.pow(x, 3) + 5 * x) || (int) (Math.pow(x, 2) - 2 * x + 4);
-//        if (x<=10 ? y==(int) (Math.pow(x, 3) + 5 * x): y==(int) (Math.pow(x, 2) - 2 * x + 4)){
-//            System.out.println(y);
-//        }
-
-//        import java.util.Scanner;
-//        public class Condition2 {
-//            public static void main(String[] args) {
-//                int x = new Scanner(System.in).nextInt();
-//                int y;
-//                if (x >= 10) {
-//                    y = (int) (Math.pow(x, 3) + 5 * x);
-//                } else {
-//                    y = (int) (Math.pow(x, 2) - 2 * x + 4);
-//                }
-//                System.out.println(y);
-//            }
-//        }
 
     }
 }
--- src/main/java/TwoMoreDimArrays.java
+++ src/main/java/TwoMoreDimArrays.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class TwoMoreDimArrays {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Eded yazin");
+        System.out.println("Enter the number");
         int number = scanner.nextInt();
         int array[][] = new int[number][number];
         for (int i = 0; i < array.length; i++) {
-
             for (int j = 0; j < array[i].length; j++) {
-                System.out.println("hjhi");
+              if ((i+j)<number){
+                  System.out.println("*");
+              }
             }
         }
         System.out.println();
