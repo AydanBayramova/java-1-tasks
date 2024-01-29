85c9014263153bf7bb79b7308ea94613b846fa8b
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-29 19:29:55 +0400

    send

--- src/main/java/lesson4/EvenOddApp.java
+++ src/main/java/lesson4/EvenOddApp.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class EvenOddApp {
     public static void main(String[] args) {
-      int a=new Scanner(System.in).nextInt();
-        System.out.println("area "+Math.pow(a,2)+"perimetr "+4*a);
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("area " + Math.pow(a, 2) + "perimetr " + 4 * a);
+        System.out.println("area circle:");
+        int r = new Scanner(System.in).nextInt();
+        System.out.println(Math.PI * 2 * r + "area" + Math.PI * Math.pow(r, 2));
 
     }
 }
