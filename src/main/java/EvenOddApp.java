589cd9b9da08606674be64ffb5d0d4d5021affb3
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-29 19:18:56 +0400

    send

---
+++ src/main/java/EvenOddApp.java
@@ -0,0 +1,10 @@
+public class EvenOddApp {
+    public static void main(String[] args) {
+        int a = 5;
+        if (a % 2 != 0) {
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+    }
+}
