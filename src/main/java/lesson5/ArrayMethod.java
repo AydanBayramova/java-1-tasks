3a6bd230f0b066f07927c6e1aab5b08b2a03d101
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-31 20:30:22 +0400

    MethodArray Task2

--- src/main/java/lesson5/ArrayMethod.java
+++ src/main/java/lesson5/ArrayMethod.java
@@ -2,10 +2,10 @@ package lesson5;
 
 public class ArrayMethod {
     public static void main(String[] args) {
-        arrayMethod(new char[]{'a','b','c'});
+        arrayChars(new char[]{'a','b','c'});
 
     }
-    public static void arrayMethod(char arr[]){
+    public static void arrayChars(char arr[]){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
