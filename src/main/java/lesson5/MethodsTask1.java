96711b4190e5b266856b91055375bc0a14119cb6
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-01-31 20:00:32 +0400

    Method Task1

--- src/main/java/lesson5/MethodsTask1.java
+++ src/main/java/lesson5/MethodsTask1.java
@@ -3,15 +3,15 @@ package lesson5;
 public class MethodsTask1 {
     public static void main(String[] args) {
 
-        double a = InputDouble(2);
-        int b = InputInteger(3);
+        double a = inputDouble(2);
+        int b = inputInteger(3);
         System.out.println(a + "  " + b);
     }
 
-    public static double InputDouble(double a) {
+    public static double inputDouble(double a) {
         return a + 2;
     }
-    public static int InputInteger(int b) {
+    public static int inputInteger(int b) {
         return b + 2;
     }
 }
