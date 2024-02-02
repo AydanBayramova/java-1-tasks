ef026d81c87747b8a202627c7337f950fad7d902
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-02-02 19:55:52 +0400

    Task

--- src/main/java/lesson6/java/CharApp.java
+++ src/main/java/lesson6/java/CharApp.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class CharApp {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        String name=scanner.nextLine();
-        String surname= scanner.nextLine();
-        String gmail=CharsApp(name);
-        int nameLength=CharsApp1(surname);
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String surname = scanner.nextLine();
+        String gmail = CharsApp(name);
+        int nameLength = CharsApp1(surname);
         System.out.println(gmail);
         System.out.println(nameLength);
 
     }
 
-    public static int CharsApp1(String name){
+    public static int CharsApp1(String name) {
         return name.length();
     }
-    public static String CharsApp(String surname){
-        return surname.replace("@"," ");
+
+    public static String CharsApp(String surname) {
+        return surname.replace("@", " ");
     }
 }
