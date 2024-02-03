6c5b161d9fafd2d47640df6d6a21b15a25485897
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-02-03 17:29:11 +0400

    task

--- src/main/java/lesson6/java/StringmethodApp.java
+++ src/main/java/lesson6/java/StringmethodApp.java
@@ -6,7 +6,7 @@ public class StringmethodApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        String email= scanner.nextLine();
+       final String email= scanner.nextLine();
         String email1 = stringApp(email);
         int nameLength = stringApp1(name);
         System.out.println(email1);
