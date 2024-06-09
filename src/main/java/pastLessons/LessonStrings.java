b6422a35b45c7cebcff6532b64b9dc599c887d63
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-06-09 16:53:58 +0400

    String Pool and Strings

---
+++ src/main/java/pastLessons/LessonStrings.java
@@ -0,0 +1,9 @@
+package pastLessons;
+
+public class LessonStrings {
+    public static void main(String[] args) {
+        String name="aydan";
+        System.out.println(name.substring(0,2));
+        System.out.println(name.length());
+    }
+}
